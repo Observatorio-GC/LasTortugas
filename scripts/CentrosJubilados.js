1a6dbd3eed4b9c495188db8e240381a78620e693
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2024-08-23 13:02:09 -0300

    Add files via upload

---
+++ scripts/CentrosJubilados.js
@@ -0,0 +1,8 @@
+var CentrosJubilados ={
+"type": "FeatureCollection",
+"name": "CentrosJubilados",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nombre_1": "Tiempo Presente", "Descrip": "Cerro Siete Colores y Penitentes" }, "geometry": { "type": "Point", "coordinates": [ -68.822741311294664, -32.943904477145757, 0.0 ] } }
+]
+}
